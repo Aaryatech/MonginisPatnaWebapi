3adbc8c51535e126b9ae98e0d968c458e64bf043
lenovo <lenovo@lenovo-Lenovo-ideapad-320-15IKB>
2019-06-05 16:39:11 +0530

    //neha

--- src/main/java/com/ats/webapi/controller/ReportController.java
+++ src/main/java/com/ats/webapi/controller/ReportController.java
@@ -10,14 +10,19 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ats.webapi.model.CustList;
 import com.ats.webapi.model.HSNWiseReport;
+import com.ats.webapi.repository.CustListRepo;
 import com.ats.webapi.repository.HSNWiseReportRepo;
 
 @RestController
 public class ReportController {
-	
+
 	@Autowired
 	HSNWiseReportRepo hSNWiseReportRepo;
+
+	@Autowired
+	CustListRepo custListRepo;
 
 	@RequestMapping(value = { "/getHsnReport" }, method = RequestMethod.POST)
 	public @ResponseBody List<HSNWiseReport> getHsnReport(@RequestParam("fromDate") String fromDate,
@@ -34,6 +39,21 @@ public class ReportController {
 		}
 
 		return saleList;
+	}
+
+	@RequestMapping(value = { "/getCutslListFroFranchasee" }, method = RequestMethod.POST)
+	public @ResponseBody List<CustList> getCutslListFroFranchasee(@RequestParam("frId") int frId) {
+
+		List<CustList> OpStockUpdateList = null;
+		try {
+
+			OpStockUpdateList = custListRepo.getOpStockAdjReportByfrId(frId);
+
+		} catch (Exception e) {
+			System.out.println(" Exce in Tax1 Report " + e.getMessage());
+			e.printStackTrace();
+		}
+		return OpStockUpdateList;
 	}
 
 }
