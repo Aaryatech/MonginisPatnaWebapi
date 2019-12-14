c1c1ddc8519b6bbb4d5165950886f72ae5b4a476
ats-12 <ats-12@ats12>
2019-12-14 14:35:30 +0530

    //Mahesh

--- src/main/java/com/ats/webapi/repository/taxreport/Tax1ReportRepository.java
+++ src/main/java/com/ats/webapi/repository/taxreport/Tax1ReportRepository.java
@@ -12,7 +12,7 @@ import com.ats.webapi.model.taxreport.Tax1Report;
 @Repository
 public interface Tax1ReportRepository extends JpaRepository<Tax1Report, Integer>{
 
-	@Query(value="SELECT\n" + 
+	/*@Query(value="SELECT\n" + 
 			"    t_bill_detail.bill_detail_no,\n" + 
 			"    t_bill_header.invoice_no,\n" + 
 			"    t_bill_header.bill_date,\n" + 
@@ -44,7 +44,41 @@ public interface Tax1ReportRepository extends JpaRepository<Tax1Report, Integer>
 			"    bill_no\n" + 
 			"ORDER BY\n" + 
 			"    t_bill_detail.bill_no,\n" + 
-			"    tax_per",nativeQuery=true)
+			"    tax_per",nativeQuery=true)*/
+	@Query(value=" Select a.bill_detail_no,a.invoice_no,a.bill_date,a.fr_name,a.fr_gst_no,a.bill_no,a.cgst_per,a.sgst_per,a.tax_per,a.taxable_amt,a.cgst_amt,a.sgst_amt,\n" + 
+			"a.total_tax,b.grand_total from (\n" + 
+			"SELECT \n" + 
+			"			    t_bill_detail.bill_detail_no, \n" + 
+			"			    t_bill_header.invoice_no,\n" + 
+			"			    t_bill_header.bill_date, \n" + 
+			"			    m_franchisee.fr_name, \n" + 
+			"			    m_franchisee.fr_gst_no, \n" + 
+			"			    t_bill_detail.bill_no, \n" + 
+			"			    t_bill_detail.cgst_per, \n" + 
+			"			    t_bill_detail.sgst_per, \n" + 
+			"			    t_bill_detail.cgst_per + sgst_per AS tax_per, \n" + 
+			"			    ROUND(SUM(t_bill_detail.taxable_amt), 2) AS taxable_amt, \n" + 
+			"			    ROUND(SUM(t_bill_detail.cgst_rs), \n" + 
+			"			    2) AS cgst_amt, \n" + 
+			"			    ROUND(SUM(t_bill_detail.sgst_rs), \n" + 
+			"			    2) AS sgst_amt, \n" + 
+			"			    ROUND(SUM(t_bill_detail.total_tax), \n" + 
+			"			    2) AS total_tax\n" + 
+			"			   \n" + 
+			"			FROM \n" + 
+			"			    t_bill_detail, \n" + 
+			"			    t_bill_header, \n" + 
+			"			    m_franchisee \n" + 
+			"			WHERE \n" + 
+			"			    t_bill_header.bill_no = t_bill_detail.bill_no AND t_bill_header.bill_date BETWEEN :fromDate AND :toDate AND m_franchisee.fr_id = t_bill_header.fr_id AND t_bill_detail.del_status = 0 AND t_bill_header.del_status = 0 \n" + 
+			"			GROUP BY \n" + 
+			"			    t_bill_detail.cgst_per + sgst_per, \n" + 
+			"			    t_bill_detail.bill_no \n" + 
+			"			ORDER BY \n" + 
+			"			    t_bill_detail.bill_no, \n" + 
+			"			    tax_per\n" + 
+			") a  left join\n" + 
+			" (select h.bill_no,ROUND(SUM(d.grand_total),2) as grand_total from t_bill_detail d, t_bill_header h WHERE d.bill_no=h.bill_no   AND  h.bill_date BETWEEN :fromDate AND :toDate AND d.del_status = 0 AND h.del_status = 0  GROUP BY h.bill_no ) b on a.bill_no=b.bill_no",nativeQuery=true)
 	List<Tax1Report> getTax1Report(@Param("fromDate")String fromDate,@Param("toDate") String toDate);
 
 	//07-12-2019
